34346b1a67b620c04eac812b8b637711e8981eca
Browny <kyul4852@gmail.com>
2021-12-16 20:20:36 +0800

    feature：home page and room card

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="room-card">
+    <div class="room-img">
+      <a class="stretched-link" :href="`#/room/${room.id}`"><img :src="room.imageUrl" alt="" /></a>
+    </div>
+    <div class="position-relative">
+      <div class="card-body card-title-container">
+        <div class="card-title fs-7 mb-0">{{ room.name }}</div>
+      </div>
+      <div class="card-body card-content-container">
+        <div class="card-content">
+          <span class="fs-5">NT.{{room.normalDayPrice}} <span class="fs-7">平日</span></span>
+          <span class="fs-7 text-muted">NT.{{room.holidayPrice}} 假日</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    room: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.room-card {
+  height: 350px;
+  width: 300px;
+  position: relative;
+  background-color: #f7f7f7;
+  box-shadow: 2px 2px 9px 0 rgba(0, 0, 0, 0.2);
+  margin-bottom: 40px;
+  .room-img {
+    height: 280px;
+    width: 100%;
+    overflow: hidden;
+  }
+  .room-img img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+  .card-body {
+    height: 70px;
+    width: 100%;
+    position: absolute;
+    // background-color: #f7f7f7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-weight: 100;
+  }
+  .card-content {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: space-between;
+  }
+  .card-title-container {
+    transition: all 0.3s;
+  }
+  .card-content-container {
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  &:hover .card-title-container {
+    transform: translateY(-65px);
+    background-color: #f7f7f7;
+  }
+  &:hover .card-content-container {
+    opacity: 1;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,301 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="banner">
+      <div class="banner-img">
+        <img src="@/assets/images/image_background.svg" alt="banner-image" />
+      </div>
+      <div class="banner-logo">
+        <img
+          class="img-fluid"
+          src="../assets/images/image_item.svg"
+          alt="logo"
+        />
+        <h1>Pilot<br />Hotel</h1>
+      </div>
+      <div class="banner-contact">
+        <div class="social-media">
+          <a href="#"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+            >
+              <defs>
+                <clipPath id="clip-Image_FB">
+                  <rect width="48" height="48" />
+                </clipPath>
+              </defs>
+              <g id="Image_FB" clip-path="url(#clip-Image_FB)">
+                <g id="Group_5" data-name="Group 5">
+                  <g
+                    id="Oval"
+                    fill="none"
+                    stroke="#fff"
+                    stroke-miterlimit="10"
+                    stroke-width="1"
+                    opacity="0.305"
+                  >
+                    <circle cx="24" cy="24" r="24" stroke="none" />
+                    <circle cx="24" cy="24" r="23.5" fill="none" />
+                  </g>
+                  <path
+                    id="facebook"
+                    d="M2.667,5.333H0V8H2.667v8h4V8H9.093l.24-2.667H6.667V4.223c0-.637.128-.889.744-.889H9.333V0H6.128c-2.4,0-3.461,1.056-3.461,3.077Z"
+                    transform="translate(19 16)"
+                    fill="#fff"
+                  />
+                </g>
+              </g>
+            </svg>
+          </a>
+          <a href="#"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+            >
+              <defs>
+                <clipPath id="clip-Image_ig">
+                  <rect width="48" height="48" />
+                </clipPath>
+              </defs>
+              <g id="Image_ig" clip-path="url(#clip-Image_ig)">
+                <g id="Group_3" data-name="Group 3" transform="translate(-112)">
+                  <g
+                    id="Oval"
+                    transform="translate(112)"
+                    fill="none"
+                    stroke="#fff"
+                    stroke-miterlimit="10"
+                    stroke-width="1"
+                    opacity="0.305"
+                  >
+                    <circle cx="24" cy="24" r="24" stroke="none" />
+                    <circle cx="24" cy="24" r="23.5" fill="none" />
+                  </g>
+                  <path
+                    id="instagram"
+                    d="M8,16c-2.144,0-2.431-.009-3.3-.049C1.79,15.818.181,14.209.048,11.3.009,10.448,0,10.165,0,8S.009,5.553.048,4.7C.181,1.792,1.79.183,4.7.05,5.563.009,5.822,0,8,0s2.455.009,3.3.05c2.912.132,4.52,1.74,4.651,4.652C15.991,5.569,16,5.856,16,8s-.009,2.432-.049,3.3c-.131,2.91-1.739,4.519-4.651,4.653C10.432,15.991,10.145,16,8,16ZM8,1.443c-2.113,0-2.376.006-3.232.045-2.169.1-3.181,1.111-3.28,3.28C1.452,5.589,1.441,5.837,1.441,8s.011,2.411.047,3.232c.1,2.168,1.113,3.18,3.28,3.28.841.039,1.093.047,3.232.047,2.115,0,2.371-.008,3.233-.047,2.168-.1,3.18-1.109,3.281-3.28.037-.848.045-1.1.045-3.232s-.008-2.382-.045-3.232C14.413,2.6,13.4,1.588,11.233,1.49,10.391,1.451,10.138,1.443,8,1.443ZM8,12.108A4.108,4.108,0,1,1,12.108,8,4.113,4.113,0,0,1,8,12.108ZM8,5.333A2.667,2.667,0,1,0,10.668,8,2.67,2.67,0,0,0,8,5.333Zm4.27-.643a.96.96,0,1,1,.961-.96A.961.961,0,0,1,12.27,4.691Z"
+                    transform="translate(128 16)"
+                    fill="#fff"
+                  />
+                </g>
+              </g>
+            </svg>
+          </a>
+        </div>
+        <div class="contact-info">
+          <p class="contact-info-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+            >
+              <defs>
+                <clipPath id="clip-Image_phone">
+                  <rect width="18" height="18" />
+                </clipPath>
+              </defs>
+              <g id="Image_phone" clip-path="url(#clip-Image_phone)">
+                <path
+                  id="Shape"
+                  d="M8.834,18h0c-1.584,0-3.377-1.173-5.05-3.3a19.445,19.445,0,0,1-3.394-6.9c-.846-3.358-.3-5.837,1.5-6.8L3.952,0,6.891,5.918,4.909,6.924c-.241.583.417,2.259,1.165,3.476.5.806.945,1.281,1.233,1.3.065-.043,1.923-.987,1.942-1l2.99,5.949-.881.445-1.2.6A3.1,3.1,0,0,1,8.834,18ZM3.287,1.947l-.743.378c-1.152.62-1.439,2.343-.807,4.85C2.8,11.4,6.179,16.514,8.774,16.514a1.83,1.83,0,0,0,.786-.174l.707-.355L8.615,12.7l-.746.374a1.151,1.151,0,0,1-.523.124,3.162,3.162,0,0,1-2.183-1.52A9.346,9.346,0,0,1,3.634,8.532c-.374-1.449-.171-2.511.558-2.914l.732-.374-1.637-3.3Zm12.42,7.8h0l-1.337-.41a5.922,5.922,0,0,0-.443-4.412,5.622,5.622,0,0,0-3.339-2.8l.4-1.382a7.017,7.017,0,0,1,4.167,3.492,7.388,7.388,0,0,1,.554,5.507Zm-2.318-.712h0l-1.3-.4A3.4,3.4,0,0,0,11.828,6.1,3.227,3.227,0,0,0,9.905,4.487l.39-1.347a4.6,4.6,0,0,1,2.732,2.289,4.845,4.845,0,0,1,.362,3.609Zm-2.284-.7h0L8.983,7.684,9.616,5.5A2.213,2.213,0,0,1,10.93,6.6a2.33,2.33,0,0,1,.175,1.737Z"
+                  transform="translate(1)"
+                  fill="#fff"
+                />
+              </g>
+            </svg>
+            <span class="ms-2">02-33408749</span>
+          </p>
+          <p class="contact-info-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+            >
+              <defs>
+                <clipPath id="clip-Image_mail">
+                  <rect width="18" height="18" />
+                </clipPath>
+              </defs>
+              <g id="Image_mail" clip-path="url(#clip-Image_mail)">
+                <path
+                  id="Shape"
+                  d="M18,14H0V0H18V14Zm-1.5-1.556V1.555H1.5l7.52,5.791L16.5,1.556,12.751,6.23Zm-15,0h15L11.606,7.3,9.022,10.006,6.458,7.265,1.5,12.444,5.288,6.223,1.5,1.556Z"
+                  transform="translate(0 2)"
+                  fill="#fff"
+                />
+              </g>
+            </svg>
+            <span class="ms-2">pilothotel@gmail.com</span>
+          </p>
+          <p class="contact-info-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="15"
+              height="18"
+              viewBox="0 0 15 18"
+            >
+              <defs>
+                <clipPath id="clip-Image_location">
+                  <rect width="15" height="18" />
+                </clipPath>
+              </defs>
+              <g id="Image_location" clip-path="url(#clip-Image_location)">
+                <path
+                  id="Shape"
+                  d="M7.5,18h0l-.142-.124C3.247,14.282,0,11.444,0,7.361A7.44,7.44,0,0,1,7.5,0,7.44,7.44,0,0,1,15,7.361c0,4.083-3.248,6.922-7.36,10.516L7.5,18Zm0-16.5a6,6,0,1,0,6,6A6.006,6.006,0,0,0,7.5,1.5Zm-.531,9.032h0L4.19,7.811,5.25,6.75,6.968,8.41,10.19,5.25l1.06,1.061-4.281,4.22Z"
+                  fill="#fff"
+                />
+              </g>
+            </svg>
+            <span class="ms-2">台北市內湖區瑞湖路四段530號</span>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="room-container">
+      <RoomCard v-for="room in roomData" :key="room.id" :room="room"></RoomCard>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RoomCard from '@/components/RoomCard.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  components: { RoomCard },
+  data () {
+    return {
+      roomData: []
+    }
+  },
+  methods: {
+    getRooms () {
+      this.axios({
+        method: 'get',
+        url: `${process.env.VUE_APP_API_PATH}/rooms`,
+        headers: {
+          Authorization: `Bearer ${process.env.VUE_APP_API_TOKEN}`
+        }
+      })
+        .then((res) => {
+          if (res.data.success) {
+            this.roomData = res.data.items
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+  created () {
+    this.getRooms()
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  height: 640px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .banner-img {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: absolute;
+    background-color: #000000;
+    z-index: -100;
+  }
+  .banner-img img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 0.6;
+  }
+  .banner-logo {
+    width: 160px;
+    height: 160px;
+    margin: 140px 0 40px;
+    position: relative;
+  }
+  .banner-logo img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -100;
+    opacity: 0.8;
+  }
+  .banner-logo h1 {
+    position: absolute;
+    bottom: 0;
+    left: 8px;
+    color: #ffffff;
+    font-size: 28px;
+  }
+  .banner-contact {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .social-media a:not(:last-child) {
+    margin-right: 10px;
+  }
+  #Image_FB:hover #Oval {
+    fill: #fff;
+  }
+  #Image_ig:hover #Oval {
+    fill: #fff;
+  }
+  .contact-info {
+    margin: 0 0 0 20px;
+    padding: 0 0 0 20px;
+    border-left: 1px solid #ffffff;
+    color: #ffffff;
+    text-align: start;
+  }
+  @include media-breakpoint-down(md) {
+    .banner-contact {
+      flex-direction: column;
+    }
+    .social-media {
+      margin-bottom: 20px;
+    }
+    .contact-info {
+      margin: 0;
+      padding: 20px 0 0 0;
+      border-left: none;
+      border-top: 1px solid #ffffff;
+      text-align: center;
+    }
+  }
+}
+.room-container {
+  max-width: 1020px;
+  transform: translateY(-60px);
+  margin: 0 auto;
+  padding: 0 20px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  @include media-breakpoint-down(lg) {
+    max-width: 680px;
+  }
+  @include media-breakpoint-down(md) {
+    max-width: 300px;
+  }
+}
+</style>
